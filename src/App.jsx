b0639191cfc49eb790450ed0e51eec0da307c2f1
yoav-katz <y0544716501@gmail.com>
2026-05-22 02:11:49 +0300

    feat(speed)

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect, lazy, Suspense } from "react";
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import Reports from "./Reports";
 import { getWorkers, getShifts, getShift, previewShift, createShift, updateShift } from "./api";
 
